34e38eecfe6be9ce3d8f1c0a528cbfdc4f0110dc
athallahannafis <athallahannafis@gmail.com>
2026-03-09 18:40:39 +0700

    feat: change title and icon  tab

---
+++ app/component/PokemonCard.tsx
@@ -0,0 +1,12 @@
+interface PokemonCardProps {
+    name: string;
+    imageUrl: string;
+    types: string[];
+}
+
+
+const PokemonCard = ({ name, imageUrl, types }: PokemonCardProps) => {
+    
+}
+
+export default PokemonCard;
