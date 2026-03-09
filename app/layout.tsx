6a1d173e21bee0ffb15598f76c63eddfac2cbbc2
athallahannafis <athallahannafis@gmail.com>
2026-03-09 19:39:43 +0700

    style: implement default font lato

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Lato } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const lato = Lato({
+  variable: "--font-lato",
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: ["400", "700"],
 });
 
 export const metadata: Metadata = {
@@ -34,7 +30,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-zinc-100`}
+        className={`${lato.variable} antialiased bg-zinc-100`}
       >
         <div className="mx-auto w-full max-w-[425px] min-w-[425px] min-h-[4000px] bg-white shadow-xl">
           {children}
