3a7005e1208a63220fa41f5b9610b8f353cfd9f7
athallahannafis <athallahannafis@gmail.com>
2026-03-09 21:51:21 +0700

    feat: types label for pokemons

--- app/component/PokemonCard.tsx
+++ app/component/PokemonCard.tsx
@@ -8,13 +8,29 @@ import { usePokemonContext } from "../context/PokemonContext";
 
 const PokemonCard = ({ pokemon }: { pokemon: PokemonCompiled }) => {
     const { addPokemonToTeam } = usePokemonContext();
-    const { name, sprite, types, exp } = pokemon;   
+    const { name, sprite, types } = pokemon;   
     return (
         <div className="p-5 h-[300px] w-[200px] flex flex-col items-center justify-between border rounded-lg border-gray-500">
             <div className="flex flex-col items-center gap-2">
-                <Image src={sprite} alt={name} width={100} height={100} />
+                <Image src={sprite} alt={name} width={110} height={110} />
                 <h2 className="text-xl font-bold">{name}</h2>
-                <p>Types: {types.map((typeInfo: TypeInfo) => typeInfo.type.name).join(", ")}</p>
+                {/* <p>
+                    Types: {types.map((typeInfo: TypeInfo) => typeInfo.type.name).join(", ")}
+
+                </p> */}
+                <div className="flex flex-row items-center justify-center gap-3 w-full">
+                    {
+                        types.map((item: TypeInfo,index : number) => {
+                            return (
+                                <div key={index}
+                                className="rounded-full px-2 font-bold border border-gray-500"
+                                >
+                                    <p>{item.type.name.charAt(0).toUpperCase() + item.type.name.slice(1)}</p>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
             </div>
             <button
                 onClick={() => addPokemonToTeam(pokemon)}
