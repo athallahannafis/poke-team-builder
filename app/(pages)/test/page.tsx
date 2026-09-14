b45d1af9b8b1fabc4bb05c74c53d210165c9470f
athallahannafis <athallahannafis@gmail.com>
2026-09-14 17:00:41 +0700

    test: changes

--- app/(pages)/test/page.tsx
+++ app/(pages)/test/page.tsx
@@ -2,7 +2,7 @@ export default function TestPage() {
   return (
     <main>
       <h1>Test Page</h1>
-      <h1>Implementasi Search bar</h1>
+      <h1>Implementasi Search bar changes</h1>
     </main>
   );
 }
